d5b1fc1f83edf999db42954a4c53f2f27d1aaa11
Dakotah Voet <dakotahvoet@gmail.com>
2020-08-22 22:04:09 -0500

    - Refactored to use CompilerServices

--- BAUnity/Trace.cs
+++ BAUnity/Trace.cs
@@ -1,122 +1,162 @@
 ﻿using System;
 using UnityEngine;
+using System.Runtime.CompilerServices;
+using System.IO;
 
 namespace BAUnity
 {
     public static class Trace
     {
-        #region Properties
-
+        #region Fields
+        // Default font size
         public static int FontSize = 12;
 
+        // Colors for the various trace statements
+        private const string COLOR_START_END = "#41a392";
         private const string COLOR_INFO = "#56d6bf";
         private const string COLOR_WARN = "#ebbe0e";
         private const string COLOR_ERROR = "#eb420e";
+        private const string COLOR_EX = "#d1155d";
         #endregion
 
-        // Log
-        public static void Log(object message)
+        #region Public
+
+        // Start
+        public static void Start(
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Debug.Log(ApplyStyles(message));
+            Debug.Log(
+                ApplyStyles(
+                    FormatCallerAttributes(sourceFilePath, memberName, sourceLineNumber),
+                    "Start",
+                    COLOR_START_END));
         }
 
-        public static void Log(object message, object obj)
+        // End
+        public static void End(
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Debug.Log(ApplyStyles(message, obj));
+            Debug.Log(
+                ApplyStyles(
+                    FormatCallerAttributes(sourceFilePath, memberName, sourceLineNumber),
+                    "End",
+                    COLOR_START_END));
+        }
+
+        // Log
+        public static void Log(
+            object message,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            Debug.Log(
+                ApplyStyles(
+                    FormatCallerAttributes(sourceFilePath, memberName, sourceLineNumber),
+                    message));
         }
 
         // Info
-        public static void Info(object message)
+        public static void Info(
+           object message,
+           [CallerMemberName] string memberName = "",
+           [CallerFilePath] string sourceFilePath = "",
+           [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Debug.Log(ApplyStyles(message, COLOR_INFO));
-        }
-
-        public static void Info(object message, object obj)
-        {
-            Debug.Log(ApplyStyles(message, COLOR_INFO, obj));
+            Debug.Log(
+                ApplyStyles(
+                    FormatCallerAttributes(sourceFilePath, memberName, sourceLineNumber),
+                    message,
+                    COLOR_INFO));
         }
 
         // Warn
-        public static void Warn(object message)
+        public static void Warn(
+          object message,
+          [CallerMemberName] string memberName = "",
+          [CallerFilePath] string sourceFilePath = "",
+          [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Debug.LogWarning(ApplyStyles(message, COLOR_WARN));
-        }
-
-        public static void Warn(object message, object obj)
-        {
-            Debug.LogWarning(ApplyStyles(message, COLOR_WARN, obj));
+            Debug.LogWarning(
+                ApplyStyles(
+                    FormatCallerAttributes(sourceFilePath, memberName, sourceLineNumber),
+                    message,
+                    COLOR_WARN));
         }
 
         // Error
-        public static void Error(object message)
+        public static void Error(
+          object message,
+          [CallerMemberName] string memberName = "",
+          [CallerFilePath] string sourceFilePath = "",
+          [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Debug.LogError(ApplyStyles(message, COLOR_ERROR));
+            Debug.LogError(
+                ApplyStyles(
+                    FormatCallerAttributes(sourceFilePath, memberName, sourceLineNumber),
+                    message,
+                    COLOR_ERROR));
         }
 
-        public static void Error(object message, object obj)
-        {
-            Debug.LogError(ApplyStyles(message, COLOR_ERROR, obj));
-        }
-
-        // Exception
+        // Exception default
         public static void Exception(Exception ex)
         {
             Debug.LogException(ex);
         }
 
-        public static void Exception(Exception ex, UnityEngine.Object obj)
+        // Exception alt
+        public static void Exception(
+          object message,
+          [CallerMemberName] string memberName = "",
+          [CallerFilePath] string sourceFilePath = "",
+          [CallerLineNumber] int sourceLineNumber = 0)
         {
-            Debug.LogException(ex, obj);
-        }
-
-        #region Apply Styles
-        private static object ApplyStyles(object message)
-        {
-            object log =
-                "<size=" + FontSize.ToString() + ">"
-                + message
-                + "</size>\n";
-
-            return log;
-        }
-
-        private static object ApplyStyles(object message, object obj)
-        {
-            object log =
-                "<size=" + FontSize.ToString() + ">"
-                + "<b>[" + obj.GetType().FullName + "]</b>: "
-                + message
-                + "</size>\n";
-
-            return log;
-        }
-
-        private static object ApplyStyles(object message, string color)
-        {
-            object log =
-                "<size=" + FontSize.ToString() + ">"
-                + "<color=" + color + ">"
-                + message
-                + "</color>"
-                + "</size>\n";
-
-            return log;
-        }
-
-        private static object ApplyStyles( object message, string color, object obj)
-        {
-            object log =
-                "<size=" + FontSize.ToString() + ">"
-                + "<color=" + color + ">"
-                + "<b>[" + obj.GetType().FullName + "]</b>: "
-                + message
-                + "</color>"
-                + "</size>\n";
-
-            return log;
+            Debug.LogError(
+                ApplyStyles(
+                    FormatCallerAttributes(sourceFilePath, memberName, sourceLineNumber),
+                    message,
+                    COLOR_EX));
         }
         #endregion
 
+        #region Private
 
+        // Apply rich text formatting
+        private static object ApplyStyles(string context, object message)
+        {
+            object log =
+                "<size=" + FontSize.ToString() + ">"
+                + "<b>[" + context + "]</b>: "
+                + message
+                + "</size>";
+
+            return log;
+        }
+
+        private static object ApplyStyles(string context, object message, string color)
+        {
+            object log =
+                "<size=" + FontSize.ToString() + ">"
+                + "<color=" + color + ">"
+                + "<b>[" + context + "]</b>: "
+                + message
+                + "</color>"
+                + "</size>";
+
+            return log;
+        }
+
+        // Concatenate and format the callerr attributes string
+        private static string FormatCallerAttributes(string path, string member, int line)
+        {
+            return Path.GetFileNameWithoutExtension(path) + ":" + member + ":" + line;
+        }
+
+        #endregion
     }
 }
+
